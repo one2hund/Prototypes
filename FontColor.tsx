c1dc9eabeef235523aebcc748ed4697e3f25f776
Jiinsung <crm03008@gmail.com>
2022-09-03 17:41:37 +0900

    회원가입 프로세스 구현80%완료, 모듈화/ 클린코드, 스타일 수정  필요

--- FontColor.tsx
+++ FontColor.tsx
@@ -2,5 +2,6 @@ export const COLORS = {
     white: '#fff',
     green: 'green',
     hand_blue : "rgb(50,171,239)",
+    hand_blue_unactivated : "rgb(152,213,247)"
   };
 
--- screens/RegisterScreen2.tsx
+++ screens/RegisterScreen2.tsx
@@ -1,10 +1,12 @@
 import { useEffect,useState,useMemo,Component } from "react";
-import { View,Text,Image,StyleSheet, TouchableOpacity,Dimensions,TextInput } from "react-native";
+import { View,Text,Image,StyleSheet, TouchableOpacity,Dimensions,TextInput, KeyboardAvoidingView } from "react-native";
 import {COLORS} from '../FontColor'
 import {Picker} from '@react-native-picker/picker';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
+
 export default function RegisterScreen2({navigation}:any)
 {
     const [step,setStep] = useState(0);
@@ -12,8 +14,12 @@ export default function RegisterScreen2({navigation}:any)
     const [birthday, onChangeBirthday] = useState("");
     const [sexcode, onChangeSexCode] = useState("");
     const [selectedLanguage, setSelectedLanguage] = useState();
-
     const [text, setText] = useState(true);
+    const [submitText, setSubmitText] = useState("본인 인증하기")
+    const [statusBarHeight, setStatusBarHeight] = useState(0)
+    const [activated, setActivated] = useState(false);
+    const buttonbgcolor = activated? COLORS.hand_blue : COLORS.hand_blue_unactivated; 
+    const [phone,setPhone] = useState("");
     const TextAnimation = () => 
     {
         if(text == true)
@@ -31,9 +37,54 @@ export default function RegisterScreen2({navigation}:any)
         setStep(1)
       else if (step == 1 && (birthday.length == 6 && sexcode.length == 1)) 
         setStep(2)
+      else if (step == 2 && (birthday.length == 6 && sexcode.length == 1 && texts.length >=2))
+        setStep(3)
+      else if (step == 3 && (birthday.length == 6 && sexcode.length == 1 && texts.length >=2 && phone.length >=11))
+        alert("본인인증 가즈아")
       else
-        alert("제대로 입력 해주세요")
+        alert("올바른 형식으로 입력 해주세요")
     }
+
+
+    useEffect(()=> {
+      if (step == 0 && texts.length >=2)
+      {
+        setSubmitText("다음")
+        setActivated(true) 
+      }
+
+      else if (step == 0 && texts.length < 2)
+      {
+        setSubmitText("본인 인증하기")
+        setActivated(false)
+      }
+
+      else if (step == 1 && birthday.length == 6 && sexcode.length == 1)
+      {
+        setSubmitText("다음")
+        setActivated(true)
+      }
+      
+      else if (step == 1 && (birthday.length < 6 || sexcode.length < 1))
+      {
+        setActivated(false)
+      }
+
+      else if (step == 2 && (birthday.length == 6 && sexcode.length == 1 && texts.length >=2))
+      {
+        setActivated(true)
+      }
+      else if (step ==3 && (birthday.length == 6 && sexcode.length == 1 && texts.length >=2 && phone.length >= 11))
+      {
+        setSubmitText("본인인증하기")
+        setActivated(true)
+      }
+      else 
+      {
+        setActivated(false)
+      }
+      
+    })
 
 
 
@@ -54,6 +105,7 @@ export default function RegisterScreen2({navigation}:any)
         {step == 0 ? <View style = {{flexDirection : "row"}}><Text style ={{...styles.signupFont, color : COLORS.hand_blue}}>이름<Text style = {styles.signupFont}>을 입력해주세요.</Text></Text></View> : null}
         {step == 1 ? <View style = {{flexDirection : "row"}}><Text style ={{...styles.signupFont, color : COLORS.hand_blue}}>주민등록번호<Text style = {styles.signupFont}>를 입력해주세요.</Text></Text></View> : null}
         {step == 2 ? <View style = {{flexDirection : "row"}}><Text style = {styles.signupFont}>사용하시는 </Text><Text style ={{...styles.signupFont, color : COLORS.hand_blue}}>통신사<Text style = {styles.signupFont}>를 선택해주세요.</Text></Text></View> : null}
+        {step == 3 ? <View style = {{flexDirection : "row"}}><Text style = {styles.signupFont}>사용하시는 </Text><Text style ={{...styles.signupFont, color : COLORS.hand_blue}}>전화번호<Text style = {styles.signupFont}>를 입력해주세요.</Text></Text></View> : null}
 
         <View style = {styles.textarea}>
         {step == 0 ? (<TextInput
@@ -61,7 +113,7 @@ export default function RegisterScreen2({navigation}:any)
       onChangeText={onChangeText}
       value={texts}
       placeholder = "이름을 입력하세요"
-    />) : null}
+    />)  : null} 
       {step == 1? <View>
         <View style = {{flexDirection : "row"}}>
           <TextInput
@@ -71,7 +123,7 @@ export default function RegisterScreen2({navigation}:any)
             <TextInput
             style={{...styles.inputbirth, width : 30, marginHorizontal : 0,}}
             keyboardType='numeric' onChangeText={onChangeSexCode}
-            value={sexcode} secureTextEntry={true} maxLength={1}/>
+            value={sexcode} maxLength={1}/>
             <Text style = {{fontSize : 10, marginTop : 12,}}>● ● ● ● ● ●</Text>
           </View>
           <TextInput
@@ -100,17 +152,47 @@ export default function RegisterScreen2({navigation}:any)
             <TextInput
             style={{...styles.inputbirth, width : 30, marginHorizontal : 0,}}
             keyboardType='numeric' onChangeText={onChangeSexCode}
-            value={sexcode} secureTextEntry={true} maxLength={1}/>
+            value={sexcode}  maxLength={1}/>
             <Text style = {{fontSize : 10, marginTop : 12,}}>● ● ● ● ● ●</Text>
           </View>
           <TextInput
             style={styles.input} onChangeText={onChangeText} value={texts} placeholder = "이름을 입력하세요" />
         </View> : null}
+        {step == 3? <View>
+          <Text  style = {styles.topic}>전화번호</Text>
+          <TextInput
+            style={styles.input} onChangeText={setPhone} value={phone} placeholder = "전화번호를 입력하세요" />
+          <View>
+          <Text  style = {styles.topic}>통신사</Text>
+          <TextInput
+            keyboardType='default' style={styles.input} value={selectedLanguage}
+            placeholder = "통신사정보" maxLength={6}/>
+          </View>
+          <Text  style = {styles.topic}>생년월일</Text>
+        <View style = {{flexDirection : "row"}}>
+          <TextInput
+            keyboardType='numeric' style={styles.inputbirth} onChangeText={onChangeBirthday} value={birthday}
+            placeholder = "생년월일 6자리" maxLength={6}/>
+          <View style = {{alignItems : "center",justifyContent : "center"}}><Text> - </Text></View>
+            <TextInput
+            style={{...styles.inputbirth, width : 30, marginHorizontal : 0,}}
+            keyboardType='numeric' onChangeText={onChangeSexCode}
+            value={sexcode}  maxLength={1}/>
+            <Text style = {{fontSize : 10, marginTop : 12,}}>● ● ● ● ● ●</Text>
+          </View>
+          <Text style = {styles.topic}>이름</Text>
+
+          <TextInput
+            style={styles.input} onChangeText={onChangeText} value={texts} placeholder = "이름을 입력하세요" />
+        </View> : null}
       </View>
         </View>
-        <View style = {styles.buttonLarge}>
-            <Text style = {styles.caption} onPress = {stepChanger}>본인 인증하기</Text> 
-        </View>        
+        
+        <KeyboardAvoidingView behavior={"padding"}  keyboardVerticalOffset = {statusBarHeight-20}>
+          <View style = {{...styles.buttonLarge, backgroundColor : buttonbgcolor}}>
+            <Text style = {styles.caption} onPress = {stepChanger}>{submitText}</Text> 
+          </View>
+        </KeyboardAvoidingView>        
     </View>
     );
 }
@@ -134,6 +216,12 @@ input: {
     },
     shadowRadius: 0,
     shadowOpacity: 1
+  },
+  topic : 
+  {
+    fontSize : 15,
+    fontWeight : "bold",
+    marginVertical : 15,
   },
   inputbirth: {
     width: 150,
@@ -221,7 +309,6 @@ buttonLarge : {
     height: windowHeight * 0.075,
     marginHorizontal : 16,
     borderRadius: 8,
-    backgroundColor: "rgb(50,171,239)",
     shadowColor: "rgba(97, 97, 97, 0.2)",
     shadowOffset: {
       width: 0,
